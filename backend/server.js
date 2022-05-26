e312f44d54b01a273f87182e38f64e387d33f6f2
edem0 <93136077+Kouvahe81@users.noreply.github.com>
2022-05-26 07:48:15 +0200

    add some files

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
 
+app.use(express.static(path.join(__dirname, "build")));
+
 //Middelware Fonction permettant d'avoir acces à la requête et sa réponse
 app.use('/',routeClient)
 app.use('/',routeFestivalDay)
